7b4bbba764161452f506164de647f4bf0f54ca1e
nishanbudhathoki2266 <nishanbudhathoki2266@gmail.com>
2023-08-24 22:14:52 +0545

    Implemented loader while changing to pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
 
 import { Open_Sans } from "next/font/google";
 import Head from "next/head";
@@ -8,6 +10,17 @@ const openSans = Open_Sans({
   subsets: ["latin"],
   variable: "--font-openSans",
 });
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#A24857",
+  className: "z-50",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 export default function App({ Component, pageProps }) {
   return (
